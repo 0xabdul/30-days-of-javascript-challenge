86307659ba3660404b4ccfd97365282172fcc230
Muhammad Abdullah <abdullahmuhammad6005@gmail.com>
2023-12-08 20:54:08 +0530

    Add files via upload

---
+++ DAY - 1/datatypes.js
@@ -0,0 +1,9 @@
+// check the data types using typeof()
+let str = 'hi man';
+console.log(typeof(str));
+let num = 30;
+console.log(typeof(num));
+console.log(typeof(true));
+console.log(typeof(null));
+let un;
+console.log(typeof(un));
---
+++ DAY - 1/variable.js
@@ -0,0 +1,39 @@
+// declare variable and assign string
+let name = 'muhammad abdullah';
+// assin boollean
+let bool = true;
+// undefined
+let my
+console.log(my);
+// null values
+let a = null;
+console.log(a);
+
+
+//declare variable without assign values
+let one;
+let two;
+let third;
+let fourth;
+
+//declare variable wit assign values
+let myname = "muhammad abdullah";
+let age = 19;
+console.log(myname +' '+ age);
+let num1 = 10;
+let num2 = 20;
+let sum = (num1+num2);
+console.log("the adding ans is :"+' '+ sum);
+//MULTI LINE
+const firstname = "muhammad";
+const lastname = "abdullah";
+const materialstatus = "none";
+const county = "india";
+const Age =20;
+// SINGLE LINE 
+const firstnam = "muhammad abdullah",job = "none",Country= "india";
+console.log(firstnam+' '+job+' '+Country); 
+// two creating variables
+const myage= 20;
+const your = 30;
+console.log("hello man my age is : "+' '+myage+' '+"and what's your age :"+' '+your);
