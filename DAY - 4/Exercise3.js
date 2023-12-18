bd350f915c4a926933aeb3d0789b1789e7ce41db
Muhammad Abdullah <abdullahmuhammad6005@gmail.com>
2023-12-18 23:21:12 +0530

    Update Exercise3.js

--- DAY - 4/Exercise3.js
+++ DAY - 4/Exercise3.js
@@ -2,7 +2,7 @@
 // There are 7 months in a year having 31 days namely January, March, Mcase 'March'ay, July, August, October, and December. 
 // here are 4 months in a year having 30 days namely April, June, September, and November.
 // The month of February has the least number of days. 28 days
-/*let months = prompt("Enter The Month Name :");
+let months = prompt("Enter The Month Name :");
 let g = months.toLowerCase();
 let b = months.toUpperCase();
 switch(months){
