7147c8e69a2c6c4a0e01c6297837a503ee4eefcf
Muhammad Abdullah <abdullahmuhammad6005@gmail.com>
2024-01-03 21:11:32 +0530

    Add files via upload

---
+++ DAY - 14/ExerciseAll.js
@@ -0,0 +1,42 @@
+// there is no exercise in 14th challenge
+// its sample code
+//problem
+try {
+    let lastName = 'Yetayeh'
+    let fullName = fistName + ' ' + lastName
+  } catch (err) {
+    console.log(err)
+  }
+  // solution
+  try {
+    let fistName = 'Asabeneh'
+    let lastName = 'Yetayeh'
+    let fullName = fistName + ' ' + lastName
+    console.log(fullName)
+  } catch (err) {
+    console.log(err)
+  }
+  // problem
+  let square = 2 x 2;
+  console.log(square)
+  console.log('Hello, world")
+
+  //solution
+  // remove x symbol
+  let squares =2*2;// change the mulitiplication symbol **
+  console.log(squares)
+  console.log("Hello, world")// change the double quatation("")
+
+  // problem
+  let num = 10// its a numeric values to change the string values
+console.log(num.toLowerCase())
+// solution
+let nums = 'ASABENEY'// its a numeric values to change the string values
+console.log(num.toLowerCase())
+// sample
+let pro = 5.44444;
+try{
+  pro = 5.44
+}catch(err){
+  console.log(err)
+}
